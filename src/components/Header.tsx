71949aa58a7696603ccbcd8f8d50314934f2af22
Jonata Silva <jonatasilva118@gmail.com>
2023-12-11 13:19:23 -0300

    fix: dashboard responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
 import Logo from "../assets/images/logo-for-darkBG.png";
-import Profile from "../assets/images/cartooon.png";
+import { SettingsMenu } from "./SettingsMenu";
 
 export function Header() {
   return (
-    <div className="flex w-full h-36 px-5 bg-primary items-center justify-between">
-      <div>
-        <h1 className="text-2xl font-semibold">Pedidos</h1>
-        <p>Acompanhe os pedidos dos clientes</p>
+    <div className="flex w-full h-36 px-5 bg-primary items-center justify-between max-md:flex-wrap-reverse max-sm:h-auto max-sm:py-5 max-sm:gap-5 max-sm:justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-2xl font-semibold text-secondary">Pedidos</h1>
+        <p className="text-secondary">Acompanhe os pedidos dos clientes</p>
       </div>
       <div className="flex w-auto gap-5 items-center justify-center">
         <img src={Logo} alt="" className="w-40" />
-        <img src={Profile} alt="" className="w-16" />
+        <SettingsMenu/>
       </div>
     </div>
   );
---
+++ src/components/SettingsMenu.tsx
@@ -0,0 +1,30 @@
+import Profile from "../assets/images/cartooon.png";
+
+export function SettingsMenu() {
+  const divStyle = {
+    backgroundImage: `url(${Profile})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+  return (
+    <div className="dropdown dropdown-bottom dropdown-end">
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn m-1 rounded-full w-16 h-16 border-hidden hover:border-solid hover:border-2 hover:border-base-100"
+        style={divStyle}>
+        </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+        <li className="menu-title">Configurações</li>
+        <li>
+          <a>Item 1</a>
+        </li>
+        <li>
+          <a>Item 2</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
