39d19b175c6638748acffb4dd1de3b761b41a7e0
JsCodeDevlopment <jonatasilva118@gmail.com>
2024-01-13 12:21:06 -0300

    refac: count button

--- src/components/QuantityButton.tsx
+++ src/components/QuantityButton.tsx
@@ -1,5 +1,3 @@
-import Add from "../assets/images/PlusLight.png";
-import Minus from "../assets/images/MinusLight.png";
 import { useState } from "react";
 
 export function QuantityButton() {
@@ -13,21 +11,55 @@ export function QuantityButton() {
     if (count > 0) {
       setCount(count - 1);
     }
-
   };
   return (
-    <div className="inline-flex rounded-md shadow-sm" role="group">
-      <button className="btn btn-circle bg-base-300"
-      onClick={handleDecrement}>
-        <img src={Minus} alt="" />
-      </button>
-      <div className="flex w-12 h-12 bg-base-300 items-center justify-center rounded-md ">
-        {count}
+    <form className="max-w-xs mx-auto">
+      <div className="relative flex items-center max-w-[8rem]">
+        <button
+          type="button"
+          onClick={handleDecrement}
+          className="bg-base-300 rounded-s-lg p-3 h-11 focus:ring-base-content focus:ring-1 focus:outline-none">
+          <svg
+            className="w-3 h-3 text-gray-900 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 2">
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h16"/>
+          </svg>
+        </button>
+        <input
+          type="number"
+          value={count}
+          onChange={(ev) => setCount(parseInt(ev.target.value, 10) || 0)}
+          className="bg-base-300 border-x-0 h-11 text-center text-sm focus:ring-base-content focus:border-base-content block w-full py-2.5"
+          placeholder="0"
+          required/>
+        <button
+          type="button"
+          onClick={handleIncrement}
+          data-input-counter-increment="quantity-input"
+          className="bg-base-300 rounded-e-lg p-3 h-11 focus:ring-base-content focus:ring-1 focus:outline-none">
+          <svg
+            className="w-3 h-3 text-gray-900 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18">
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"/>
+          </svg>
+        </button>
       </div>
-      <button className="btn btn-circle bg-base-300"
-      onClick={handleIncrement}>
-        <img src={Add} alt="" />
-      </button>
-    </div>
+    </form>
   );
 }
