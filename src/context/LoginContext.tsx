01071d0e051d841337998091b57644c86860aba3
JsCodeDevlopment <jonatasilva118@gmail.com>
2024-01-18 12:57:08 -0300

    fix: removed toast at login page

--- src/context/LoginContext.tsx
+++ src/context/LoginContext.tsx
@@ -1,6 +1,12 @@
-import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { baseURL } from "../servises/BackEndBaseURL";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { ILogedUser } from "../interfaces/ILogedUser";
 import { ILoginContext } from "../interfaces/ILoginContext";
@@ -11,8 +17,12 @@ export const LoginProvider = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [logedUser, setLogedUser] = useState<ILogedUser>();
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const login = async ( email: string, password: string ): Promise<void | boolean> => {
+  const login = async (
+    email: string,
+    password: string
+  ): Promise<void | boolean> => {
     try {
       const response = await fetch(`${baseURL}/login`, {
         method: "POST",
@@ -22,10 +32,13 @@ export const LoginProvider = ({ children }: { children: ReactNode }) => {
         body: JSON.stringify({ email, password }),
       });
       const data = await response.json();
-    
-      setLogedUser(data);
-      if (data) {
+      if (response.ok) {
+        setLogedUser(data);
         setIsAuthenticated(true);
+      } else {
+        toast.error("Acesso negado, senha ou email incorretos.", {
+          autoClose: 1000 * 3,
+        });
       }
     } catch (error) {
       console.error(error, "Erro ao fazer a requisição.");
@@ -46,7 +59,7 @@ export const LoginProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     if (isAuthenticated) {
       navigate("/dashboard", { replace: true });
-    } else {
+    } else if (location.pathname !== "/") {
       toast.error("Acesso negado, tente logar novamente.", {
         autoClose: 1000 * 3,
       });
